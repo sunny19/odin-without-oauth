b6cb68fdfa737a7e6a92938be33e6a0394fbcf7a
sunnynk19 <sunnynk19@gmail.com>
2017-03-02 16:38:50 +0530

    Subdomain removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Solution of https://gist.github.com/indiesquidge/b836647f851179589765
+  config.action_dispatch.tld_length = 0
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
